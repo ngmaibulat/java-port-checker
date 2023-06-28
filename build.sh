bdce29cb0848fe1eaf8a2b0b9898d3759a111424
ngm <ngmaibulat@gmail.com>
2023-06-28 17:26:43 +0300

    restructure

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 ./gradlew build
 
-rm -f build/libs/tcpping.jar
+rm -fr install
+mkdir install
 
-mv build/libs/tcpping-all.jar build/libs/tcpping.jar
+cp build/libs/tcpping-all.jar install/tcpping.jar
 
 # jpackage --type app-image --input build/libs --main-jar tcpping.jar --name tcpping
 # jpackage --type dmg --input build/libs --main-jar tcpping.jar --name tcpping
-jpackage --type pkg --input build/libs --main-jar tcpping.jar --name tcpping --app-version 1.0.0
+jpackage --type pkg --input install --main-jar tcpping.jar --name tcpping --app-version 1.0.0
+mv tcpping-*.pkg install/
 
 # hdiutil create -format UDZO -srcfolder tcpping.app tcpping.dmg
-
 
 # jpackage --input target/ \
 #   --name MyAppName \
